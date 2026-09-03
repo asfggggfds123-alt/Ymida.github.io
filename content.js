4a705c38d69291cf2a698c1e07fb86389409f585
suntoch <asfggggfds123@gmail.com>
2026-09-03 11:47:10 +0300

    Add files via upload

--- content.js
+++ content.js
@@ -8,6 +8,6 @@ const siteContent = {
         { name: 'MAX', icon: 'fas fa-comment-dots', link: 'https://max.ru/u/f9LHodD0cOLXxyk2r3Dbf8MwTvXlQ-OnUwMfm3aunht6aWm54ycBamaEMI4' },
         { name: 'Telegram', icon: 'fab fa-telegram', link: 'https://t.me/Umidacentr' },
         { name: 'WhatsApp', icon: 'fab fa-whatsapp', link: 'https://wa.me/79063229333' },
-        { name: 'ВКонтакте', icon: 'fab fa-vk', link: 'https://vk.com/yumida' }
+        { name: 'ВКонтакте', icon: 'fab fa-vk', link: 'https://vk.ru/club240774245' }
     ]
 };
